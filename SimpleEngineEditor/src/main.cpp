a792978d7aa4b14d7151581159e816fe558cd67e
Aleksey <medvedev_a_l@mail.ru>
2022-12-08 04:54:05 +0500

    add glfw library

---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "SimpleEngineCore/Application.hpp"
+#include <memory>
+
+class MyApp : public SimpleEngine::Application
+{
+    virtual void on_update() override
+    {
+        std::cout << "Update frame:" << ++frame << std::endl;
+    };
+
+    int frame = 0;
+};
+
+int main()
+{
+   auto myApp = std::make_unique<MyApp>();
+
+   int returnCode = myApp -> start(1024, 768, "My first app");
+
+   std::cin.get();
+   return returnCode;
+}
