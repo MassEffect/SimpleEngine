650dc86a9a69577563b4655f10f377281348b492
Aleksey <medvedev_a_l@mail.ru>
2022-12-25 11:22:21 +0500

    Euler angles

--- SimpleEngineCore/includes/SimpleEngineCore/Camera.hpp
+++ SimpleEngineCore/includes/SimpleEngineCore/Camera.hpp
@@ -22,17 +22,37 @@ namespace SimpleEngine
         void set_rotation(const glm::vec3& rotation);
         void set_position_rotation(const glm::vec3& postion, const glm::vec3& rotation);
         void set_projection_mode(const ProjectionMode projection_mode);
-
         glm::mat4 get_view_matrix()const {return m_view_matrix;};
         glm::mat4 get_projection_matrix()const {return m_projection_matrix;};
+
+        void move_forward(const float delta);
+        void move_right(const float delta);
+        void move_up(const float delta);
+
+        const glm::vec3& get_camera_position()const {return m_position;};
+        const glm::vec3& get_camera_rotation()const {return m_rotation;};
+
+        //movement delta.x - forward, movement delta.y - right, movement delta.z - up
+        //rotation delta.x - roll, rotation delta.y - pitch, rotation delta.z - yaw
+        void add_movement_and_rotation(const glm::vec3& movement_delta,
+                                       const glm::vec3& rotation_delta);
 
     private:
         void update_view_matrix();
         void update_projection_matrix();
 
         glm::vec3 m_position;
-        glm::vec3 m_rotation;
+        glm::vec3 m_rotation; // X - roll, Y - pitch, Z - yaw
         ProjectionMode m_projection_mode;
+
+        glm::vec3 m_direction;
+        glm::vec3 m_right;
+        glm::vec3 m_up;
+
+        static constexpr glm::vec3 s_world_up{0.f, 0.f, 1.f};
+        static constexpr glm::vec3 s_world_right{0.f, -1.f, 0.f};
+        static constexpr glm::vec3 s_world_forward{1.f, 0.f, 0.f};
+
         glm::mat4 m_view_matrix;
         glm::mat4 m_projection_matrix;
     };
