fab494c8a6728af6b8ab45c084f948565a68ed1e
Aleksey <medvedev_a_l@mail.ru>
2022-12-28 16:30:23 +0500

    Added depth buffer

--- SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/Renderer_OpenGL.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/Renderer_OpenGL.cpp
@@ -36,7 +36,7 @@ namespace SimpleEngine
 
     void Renderer_OpenGL::clear()
     {
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     };
 
     void Renderer_OpenGL::set_viewport(const unsigned int width, const unsigned int height, const unsigned int left_offset, const unsigned int bottom_offset)
@@ -57,5 +57,15 @@ namespace SimpleEngine
     const char* Renderer_OpenGL::get_version_str()
     {
         return reinterpret_cast<const char*>(glGetString(GL_VERSION));
+    };
+
+    void Renderer_OpenGL::enable_depth_test()
+    {
+        glEnable(GL_DEPTH_TEST);
+    };
+
+    void Renderer_OpenGL::disable_depth_test()
+    {
+        glDisable(GL_DEPTH_TEST);
     };
 }
