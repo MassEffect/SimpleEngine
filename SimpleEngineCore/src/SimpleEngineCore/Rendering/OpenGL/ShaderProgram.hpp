73fb32a17398ebb0bbc7b0a9276444520f569829
Aleksey <medvedev_a_l@mail.ru>
2022-12-31 08:40:48 +0500

    Added Fong lighting model

--- SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/ShaderProgram.hpp
+++ SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/ShaderProgram.hpp
@@ -22,6 +22,8 @@ namespace SimpleEngine
         bool is_compiled() { return m_isCompiled;};
         void set_matrix4(const char* name, const glm::mat4& matrix)const;
         void set_int(const char* name, const int value)const;
+        void set_float(const char* name, const float value)const;
+        void set_vec3(const char* name, const glm::vec3& value)const;
 
     private:
         bool m_isCompiled = false;
